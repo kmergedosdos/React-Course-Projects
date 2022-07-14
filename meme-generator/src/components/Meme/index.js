3ded6d664dc933794d5ad8573d2435691a6260ed
Kmerge Boi <kpdosdos@gmail.com>
2022-07-14 17:13:20 +0800

    feat: add text input to image

--- meme-generator/src/components/Meme/index.js
+++ meme-generator/src/components/Meme/index.js
@@ -3,13 +3,14 @@ import './index.css';
 import memesData from '../../memesData';
 
 const Meme = () => {
-   const [meme, setMeme] = useState({
+   const [memeData, setMeme] = useState({
       topText: "",
       bottomText: "",
       imgUrl: ""
    });
 
    const memes = memesData.data.memes;
+
    function getNewMemeImage(e) {
       e.preventDefault();
       const newMeme = memes[Math.floor(Math.random() * memes.length)];
@@ -20,22 +21,40 @@ const Meme = () => {
       }));
    }
 
+   function handleChange(event) {
+      const { name, value } = event.target;
+      setMeme(prevMemeData => ({
+         ...prevMemeData,
+         [name]: value
+      }));
+   }
+
    return (
       <main className="Meme">
-         <form className="form">
+         <form className="form" onSubmit={getNewMemeImage}>
             <input
                type="text"
+               name="topText"
+               value={memeData.topText}
+               onChange={handleChange}
                placeholder="Top Text"
                className="form--input"
             />
             <input
                type="text"
+               name="bottomText"
+               value={memeData.bottomText}
+               onChange={handleChange}
                placeholder="Bottom Text"
                className="form--input"
             />
-            <button className="form--submit" onClick={getNewMemeImage}>Get a new meme image 🖼</button>
+            <button className="form--submit">Get a new meme image 🖼</button>
          </form>
-         {meme.imgUrl !== "" && <img src={meme.imgUrl} alt="New Meme" className="meme--img"/>}
+         <div className='meme--container'>
+            {memeData.imgUrl !== "" && <img src={memeData.imgUrl} alt="New Meme" className="meme--img"/>}
+            <h2 className='meme--toptext'>{memeData.imgUrl !== "" && memeData.topText.toUpperCase()}</h2>
+            <h2 className='meme--bottomtext'>{memeData.imgUrl !== "" && memeData.bottomText.toUpperCase()}</h2>
+         </div>
       </main>
    );
 }
