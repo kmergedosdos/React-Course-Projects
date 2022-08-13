ffe3ec72725856abbee151e5f29c63b17a5346dc
Kmerge Boi <kpdosdos@gmail.com>
2022-08-13 17:03:56 +0800

    feat: retrieve trivia items from trivia api

--- quizzical/src/App.js
+++ quizzical/src/App.js
@@ -2,11 +2,21 @@ import Portal from "./components/Portal";
 import Item from "./components/Item";
 import BlobTopRight from "./images/blob-top-right.png";
 import BlobBotLeft from "./images/blob-bot-left.png";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
 
   const [start, setStart] = useState(false);
+  const [triviaData, setTriviaData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple')
+      .then(res => res.json())
+      .then(data => {
+        setTriviaData(data.results);
+      })
+      .catch(err => console.log(err))
+  }, [])
 
   return (
     <div className="App">
@@ -17,11 +27,16 @@ function App() {
         <Portal startQuiz={() => {setStart(true)}}/> :
         <div className="test-page">
           <div className="items-container">
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
+            {
+              triviaData.map((item, i) => (
+                <Item
+                  key={i}
+                  question={item.question}
+                  correct_answer={item.correct_answer}
+                  incorrect_answers={item.incorrect_answers}
+                />
+              ))
+            }
           </div>
           <div>
             <button className="check-button">Check answers</button>
--- quizzical/src/components/Item.js
+++ quizzical/src/components/Item.js
@@ -1,12 +1,54 @@
-const Item = () => {
+import { useState, useEffect } from 'react';
+
+const Item = ({ question, correct_answer, incorrect_answers}) => {
+   const [choices, setChoices] = useState([])
+
+   useEffect(() => {
+      let tempChoices = [
+         {
+            value: correct_answer,
+            selected: false
+         },
+         {
+            value: incorrect_answers[0],
+            selected: false
+         },
+         {
+            value: incorrect_answers[1],
+            selected: false
+         },
+         {
+            value: incorrect_answers[2],
+            selected: false
+         }
+      ];
+      setChoices(tempChoices.sort(() => 0.5 - Math.random()));
+   }, [correct_answer, incorrect_answers])
+
+   function select(value) {
+      setChoices(prevChoices => (
+         prevChoices.map(choice => ({
+            ...choice,
+            selected: choice.value === value ? true : false
+         }))
+      ))
+   }
+
    return (
       <div className="item">
-         <h2 className="question">How would one say goodbye in Spanish?</h2>
+         <h2 className="question">{question}</h2>
          <ul className="choices">
-            <li className="active">Adiós</li>
-            <li>Hola</li>
-            <li>Au Revoir</li>
-            <li>Salir</li>
+            {
+               choices.map(choice => (
+                  <li
+                     key={choice.value}
+                     className={choice.selected ? "active" : ""}
+                     onClick={() => select(choice.value)}
+                  >
+                     {choice.value}
+                  </li>
+               ))
+            }
          </ul>
          <hr/>
       </div>
